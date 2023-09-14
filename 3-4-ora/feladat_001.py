5f18109f151e0c4b0b98e69fe6c64efbc374abab
korossybence <125134834+korossybence@users.noreply.github.com>
2023-09-14 10:28:10 +0200

    Add files via upload

---
+++ 3-4-ora/feladat_001.py
@@ -0,0 +1,21 @@
+# feladat_001
+"""
+Kérjük be a billenytűzetről a nevünket, és irassuk ki a képernyőre!
+a képet a billenytűzetről mindig szöveget (string-et) olvasunk be!
+type(változó)
+"""
+
+'''
+nev = input("kérek egy nevet: ")
+print(f"A megadott név a következő: {nev}")
+'''
+
+vnev = input("kérek egy vezetéknevet: ")
+knev = input("kérek egy keresztnevet: ")
+print(f"A megadott név a következő: {vnev} {knev}")
+
+
+
+
+print(f"A vnev változó típusa: {type(vnev)}")
+print(f"A knev változó típusa: {type(knev)}")
---
+++ 3-4-ora/feladat_002.py
@@ -0,0 +1,11 @@
+#feladat_002
+"""
+Kérjünk be a billenytűzetről két egész számot és
+irassuk ki a két szám összegét a képernyőre! 
+"""
+szam_1 = input("kérek egy számot: ")
+szam_1 = input(szam_1)
+szam_2 = int(input("kérek egy másik számot: "))
+osszeg = szam_1 + szam_2
+print(f"A megadott két szám összege: {szam_1 + szam_2}")
+print(f"A megadott két szám összege: {osszeg}")
